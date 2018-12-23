524b8aaae2f2d9ea315451d878cbde65156cca7a
gw <likelightguang@163.com>
2018-12-23 16:39:48 +0800

    Update myResNet.py

--- cifar/myResNet.py
+++ cifar/myResNet.py
@@ -9,10 +9,10 @@ class ResNetI(nn.Module):
 	"""docstring for ResNetI"""
 	def __init__(self, arg):
 		super(ResNetI, self).__init__()
-		self.conv1 = nn. 
+		self.conv1 = nn.Conv2d(1,)
 
 	def Conv3x3(self,input_channels,output_chanels,n):
 		for i in range(n):
 			x=nn.Conv2d(input_channels,output_chanels,3,1,1)
-			x=nn.Conv3d
+
 		return x+
